87473bd60798d1dea7ea6938e7784d806d6dd435
MattDobsonWeb <hello@mattdobson.me>
2021-12-28 11:50:53 +0000

    Lesson 2 - Connect to the database

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -10,14 +11,9 @@ app.get("/", (req, res) => {
   res.send("Fullstack React Tutorial Express Server");
 });
 
-app.post("/name", (req, res) => {
-  if (req.body.name) {
-    return res.json({ name: req.body.name });
-  } else {
-    return res.status(400).json({ error: "No name provided" });
-  }
-});
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log("Connected to database");
+  app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+  });
 });
