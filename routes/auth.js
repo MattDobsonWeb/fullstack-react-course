a40213ee3af094b389a8c56154a929b20549a9dd
MattDobsonWeb <hello@mattdobson.me>
2021-12-28 17:25:20 +0000

    Lesson 5 - User auth

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 const validateRegisterInput = require("../validation/registerValidation");
+const requiresAuth = require("../middleware/permissions");
+const jwt = require("jsonwebtoken");
 
 // @route   GET api/auth/test
 // @desc    Test the auth route
@@ -56,6 +58,78 @@ router.post("/register", async (req, res) => {
     console.log(err);
 
     res.status(500).send(err.message);
+  }
+});
+
+// @route   POST api/auth/login
+// @desc    Login User / Returning JWT Token
+// @access  Public
+router.post("/login", async (req, res) => {
+  try {
+    // check for user with email
+    const user = await User.findOne({
+      email: new RegExp("^" + req.body.email + "$", "i"),
+    });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ error: "There was a problem with your login credentials" });
+    }
+
+    const passwordMatch = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    if (!passwordMatch) {
+      return res.status(400).json({
+        error: "There was a problem with your login credentials.",
+      });
+    }
+
+    const payload = { userId: user._id };
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("access-token", token, {
+      expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    console.log(process.env.NODE_ENV);
+
+    const userToReturn = { ...user._doc };
+    delete userToReturn.password;
+
+    return res.json({
+      token: `${token}`,
+      user: userToReturn,
+    });
+  } catch (err) {
+    // error here
+    console.log(err);
+
+    res.status(500).send(err.message);
+  }
+});
+
+// @route   POST api/auth/current
+// @desc    Return the current user
+// @access  Private
+router.get("/current", requiresAuth, async (req, res) => {
+  try {
+    if (!req.user) {
+      return res.status(401).send("Unauthorized");
+    }
+
+    return res.json(req.user);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: err.message });
   }
 });
 
